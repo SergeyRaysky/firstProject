c7183f3b2c0bfa74f44ab490cc3be2ae32451cf0
Сергей Райский <sergejklincevic04@gmail.com>
2024-02-08 12:15:01 +0300

    Создал detailViewModel и доделал сортировку

--- app/src/main/java/com/example/myfirstapp/ViewModel.kt
+++ app/src/main/java/com/example/myfirstapp/ViewModel.kt
@@ -5,9 +5,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel : ViewModel() {
 
-        private  val colors = listOf(
+    private val colors = listOf(
         Color.RED,
         Color.GREEN,
         Color.YELLOW,
@@ -19,36 +19,34 @@ class HomeViewModel: ViewModel() {
         Color.MAGENTA,
         Color.TRANSPARENT
     )
-    var sortFlag = mutableStateOf(false)
+    var sortFlag = mutableStateOf(true)
 
-    var data = mutableStateOf( createList(10))
-    fun refreshData(){
+    var data = mutableStateOf(createList(10))
+    fun refreshData() {
         data.value = createList(10)
     }
 
-    private fun createList(size: Int)=
+    private fun createList(size: Int) =
         MutableList(size) {
 
             Item(
                 id = it,
                 color = colors.random(),
-                key = Random.nextInt(0,100)
+                key = Random.nextInt(0, 100)
             )
 
         }
 
-    fun sortedKey(){
+    fun sortedKey() {
         val list = mutableListOf<Item>()
         list.addAll(data.value)
-        if(!sortFlag.value) {
+        if (sortFlag.value) {
             list.sortBy { it.key }
-            sortFlag.value = true
+        } else {
+            list.sortByDescending { it.key }
         }
-        else  {
-        list.sortByDescending { it.key }
-        sortFlag.value = false
-            }
+        sortFlag.value = !sortFlag.value
         data.value = list
     }
-    }
+}
 
---
+++ app/src/main/java/com/example/myfirstapp/detailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myfirstapp
+
+import androidx.lifecycle.ViewModel
+
+class detailViewModel : ViewModel() {
+
+}
